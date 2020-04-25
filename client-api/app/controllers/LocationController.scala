6d56c3752e3f6e662c80199b21f8c226d1ef9307
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-04-25 17:02:32 +0300

    Added more methods to location controller

--- client-api/app/controllers/LocationController.scala
+++ client-api/app/controllers/LocationController.scala
@@ -5,22 +5,53 @@ import models.LocationAggregation
 import play.api._
 import play.api.mvc._
 import play.api.libs.json.Json
+import scala.collection.mutable
 
 @Singleton
 class LocationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
     
-    val tempLocation = new LocationAggregation("myTestLocation", 941, 223)
+    val aggMap = mutable.Map.empty[String,LocationAggregation]
 
-    def aggregationByLocation(locationname: String) = Action {
-        val locationJson = Json.toJson(tempLocation)
-        // toJson(data) relies on existence of
-        // `Writes[List[Repo]]` type class in scope
+
+    def getListOfKnownLocations() = Action {
+        val locationsJson = Json.toJson(aggMap.keySet)
+        Ok(locationsJson)
+    }
+
+    def getAggregationByLocation(locationname: String) = Action {
+        if (aggMap.contains(locationname)) {
+            val locationJson = Json.toJson(aggMap(locationname))
+            Ok(locationJson)
+        }
+        else {
+            NotFound(locationname)
+        }
+    }
+
+    def getAllAggregations() = Action {
+        val locationJson = Json.toJson(aggMap.toList)
         Ok(locationJson)
     }
 
-    def getListOfKnownLocations() = Action {
-        val locations = List("Iiyama", "Onomichi", "Odaiba")
-        val locationsJson = Json.toJson(locations)
-        Ok(locationsJson)
+    def getTotalEvents() = Action {
+        val total = aggMap.values.map(x => x.failed + x.valid).sum
+        Ok(total.toString())
+    }
+
+    def newLocationAggregation(locationname: String) = Action {
+        aggMap += (locationname -> new LocationAggregation(locationname, 941, 223))
+        Ok
+    }
+
+    def removeAggregation(locationname: String) = Action {
+        aggMap.remove(locationname) match {
+            case Some(value) => Ok(s"Removed aggregation for '${value.location}'")
+            case None => NotFound(locationname)
+        }
+    }
+
+    def clearAllAggregations() = Action {
+        aggMap.clear
+        Ok
     }
 }
