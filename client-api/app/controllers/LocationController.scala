a3481a10bb4213a53c2e9a60754caaa051e338cd
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-04-25 15:16:04 +0300

    List of locations
    API method

--- client-api/app/controllers/LocationController.scala
+++ client-api/app/controllers/LocationController.scala
@@ -4,27 +4,23 @@ import javax.inject._
 import models.LocationAggregation
 import play.api._
 import play.api.mvc._
-import play.api.libs.json._
+import play.api.libs.json.Json
 
 @Singleton
 class LocationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
     
     val tempLocation = new LocationAggregation("myTestLocation", 941, 223)
 
-    // Typeclass for converting LocationAggregation -> JSON
-    implicit val writesLocationAggregation = new Writes[LocationAggregation] {
-        def writes(agg:LocationAggregation) = Json.obj(
-            "location" -> agg.location,
-            "valid" -> agg.valid,
-            "failed" -> agg.failed
-        )
-    }
-
-
     def aggregationByLocation(locationname: String) = Action {
         val locationJson = Json.toJson(tempLocation)
         // toJson(data) relies on existence of
         // `Writes[List[Repo]]` type class in scope
         Ok(locationJson)
+    }
+
+    def getListOfKnownLocations() = Action {
+        val locations = List("Iiyama", "Onomichi", "Odaiba")
+        val locationsJson = Json.toJson(locations)
+        Ok(locationsJson)
     }
 }
--- client-api/app/models/LocationAggregation.scala
+++ client-api/app/models/LocationAggregation.scala
@@ -1,7 +1,30 @@
 package models
 
+import play.api.libs.json.Json
+import play.api.libs.json.Format
+import play.api.libs.json.{JsResult, JsValue}
+import play.api.libs.json.JsSuccess
+
 final class LocationAggregation(
     val location: String,
     val valid: Long,
     val failed: Long
 )
+
+object LocationAggregation {
+
+    implicit object LocationAggregationFormat extends Format[LocationAggregation] {
+
+        // convert LocationAggregation to Json (serialise)
+        def writes(agg:LocationAggregation) = Json.obj(
+            "location" -> agg.location,
+            "valid" -> agg.valid,
+            "failed" -> agg.failed
+        )
+
+        // TODO implement deserialisation
+        override def reads(json: JsValue): JsResult[LocationAggregation] = {
+            JsSuccess(new LocationAggregation("", 0, 0))
+        }
+    }
+}
