adc5ab7f91f6058d37ad652a31a63852d8afb997
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-04-26 13:51:30 +0300

    signal parsing logic

--- signal-api/app/controllers/SignalController.scala
+++ signal-api/app/controllers/SignalController.scala
@@ -5,22 +5,27 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import models.SignalEvent
 
 @Singleton
 class SignalController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   def newSignalEvent() = Action { request =>
-    request.body.asJson.map { json =>
-
-      json.validate[(String, String, String, String)].map { 
-        case (location, attribute, uom, value) => Ok("Hello " + location + ", you're "+attribute)
-      }.recoverTotal{
-        e => BadRequest("Detected error:" + JsError(e.toString()))
+    request.body.asJson match {
+      case Some(jsonValue) => {
+        val parseResult = Json.fromJson[SignalEvent](jsonValue)
+        parseResult match {
+          case JsSuccess(value, path) => {
+            println(s"Got signal event from request:\nlocation: ${value.location}\nattribute: ${value.attribute}")
+            // TODO handle signal event
+            Ok
+          }
+          case JsError(errors) => {
+            BadRequest("Detected error:" + JsError(errors.take(0).toString()))
+          }
+        }
       }
-
-    }.getOrElse {
-      BadRequest("Expecting Json data")
+      case None => BadRequest("Expecting Json data")
     }
   }
-
 }
--- signal-api/app/models/SignalEvent.scala
+++ signal-api/app/models/SignalEvent.scala
@@ -1,11 +1,16 @@
 package models
 
 import java.time.LocalDateTime
+import play.api.libs.json.Json
 
 final case class SignalEvent(
     location: String,
     attribute: String,
     uom: String, // Unit of Measure
-    value: String,
-    timestamp: LocalDateTime
+    value: String//,
+    //timestamp: LocalDateTime
 )
+
+object SignalEvent {
+    implicit val signalJsonFormat = Json.format[SignalEvent]
+}
