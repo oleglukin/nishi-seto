150ce545105aed8b9d1a4cb008e6795f2db4b912
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-04-28 14:54:28 +0300

    Updated eventsource logic

--- eventsource/src/main/scala/eventsource/EventSourceApp.scala
+++ eventsource/src/main/scala/eventsource/EventSourceApp.scala
@@ -3,10 +3,8 @@ package eventsource
 import scala.collection.immutable.NumericRange
 
 object EventSourceApp {
-  val chLower = ('a' to 'z')
   val chUpper = ('A' to 'Z')
   val chNum = ('0' to '9')
-  val chAlphaNum = chLower ++ chUpper ++ chNum
 
   def main(args: Array[String]): Unit = {
 
@@ -24,28 +22,22 @@ object EventSourceApp {
     val events = 16;
     val maxeventSourceIds = 10;
 
-    getSources(maxeventSourceIds).foreach(println)
+    val sourcesIds = getSources(maxeventSourceIds)
 
+    sourcesIds.foreach(println)
 
   }
 
 
   def getSources(maxSources: Int) = 
     (0 to (maxSources - 1)).map(_ match {
-      case a if a%3 == 0 => getStringUpperCase(3) + "_" + getStringUpperCase(8)
-      case b => getString(4, ('D' to 'R')) + "_" + getStringUpperCase(3) + getStringNumeric(3) + getStringUpperCase(2)
+      case a if a%3 == 0 => "EX" + getStringUpperCase(2) + "_" + getStringUpperCase(8)
+      case b => "TR" + getString(2, ('D' to 'R')) + "_" + getStringUpperCase(3) + getStringNumeric(3) + getStringUpperCase(2)
     })
 
-  def getString(length: Int, chars: NumericRange[Char]): String = {
-    //val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
-
-    (0 to (length - 1)).map(i => {
-      chars(util.Random.nextInt(chars.length))
-    }).mkString
-  }
-
-  def getStringLowerCase(length: Int): String = getString(length, chLower)
-  def getStringUpperCase(length: Int): String = getString(length, chUpper)
-  def getStringNumeric(length: Int): String = getString(length, chNum)
-  
+  def getString(length: Int, chars: IndexedSeq[Char]) = (1 to length).map(_ => getRandomChar(chars)).mkString
+  def getRandomChar(chars: IndexedSeq[Char]) = chars(util.Random.nextInt(chars.length))
+  def getStringUpperCase(length: Int) = getString(length, chUpper)
+  def getStringNumeric(length: Int) = getString(length, chNum)
+  def getStringUpperCaseAlphaNumeric(length: Int) = getString(length, (chUpper ++ chNum))
 }
