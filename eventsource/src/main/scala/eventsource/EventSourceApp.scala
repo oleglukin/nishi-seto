df92e6f147be1c01bbde8f8a048a522885d0bb3a
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-04-28 14:41:02 +0300

    Added eventsource logic

--- eventsource/src/main/scala/eventsource/EventSourceApp.scala
+++ eventsource/src/main/scala/eventsource/EventSourceApp.scala
@@ -1,6 +1,13 @@
 package eventsource
 
+import scala.collection.immutable.NumericRange
+
 object EventSourceApp {
+  val chLower = ('a' to 'z')
+  val chUpper = ('A' to 'Z')
+  val chNum = ('0' to '9')
+  val chAlphaNum = chLower ++ chUpper ++ chNum
+
   def main(args: Array[String]): Unit = {
 
     // TODO read config from file
@@ -15,16 +22,30 @@ object EventSourceApp {
     )
 
     val events = 16;
-    val maxeventSourceIds = 3;
+    val maxeventSourceIds = 10;
 
-
+    getSources(maxeventSourceIds).foreach(println)
 
 
   }
 
 
-  def getLocations(maxLocations: Int): Seq[String] = {
-    (0 to (maxLocations - 1)).foreach()
+  def getSources(maxSources: Int) = 
+    (0 to (maxSources - 1)).map(_ match {
+      case a if a%3 == 0 => getStringUpperCase(3) + "_" + getStringUpperCase(8)
+      case b => getString(4, ('D' to 'R')) + "_" + getStringUpperCase(3) + getStringNumeric(3) + getStringUpperCase(2)
+    })
+
+  def getString(length: Int, chars: NumericRange[Char]): String = {
+    //val chars = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
+
+    (0 to (length - 1)).map(i => {
+      chars(util.Random.nextInt(chars.length))
+    }).mkString
   }
+
+  def getStringLowerCase(length: Int): String = getString(length, chLower)
+  def getStringUpperCase(length: Int): String = getString(length, chUpper)
+  def getStringNumeric(length: Int): String = getString(length, chNum)
   
 }
--- signal-api/app/models/SignalEvent.scala
+++ signal-api/app/models/SignalEvent.scala
@@ -4,10 +4,10 @@ import java.time.LocalDateTime
 import play.api.libs.json.Json
 
 final case class SignalEvent(
-    source: String, // signal source id/name
-    attribute: String,
-    uom: String, // Unit of Measure
-    value: String//,
+    source: String, // signal source id/name, e.g. ZXCP_HRT059NC
+    attribute: String, // attribute name, e.g. temperature or engine speed
+    uom: String,    // Unit of Measure, e.g. rpm, percentage
+    value: String   // e.g. 16, 954.12, or "read failure"
     //timestamp: LocalDateTime
 )
 
