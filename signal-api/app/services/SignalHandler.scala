b455f5e0f4ee85c25e3c8d9c4239b61361b6ac22
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-05-06 15:12:45 +0300

    signal api: fixed filename

--- signal-api/app/services/SignalHandler.scala
+++ signal-api/app/services/SignalHandler.scala
@@ -31,8 +31,17 @@ class SignalHandler(exchangeFolder: String) extends Actor {
         }
     }
 
-    def getFilePath = {
-        val now = LocalDateTime.now
-        s"$exchangeFolder${now.getYear}-${now.getMonthValue}-${now.getDayOfMonth()}_${now.getHour}-${now.getMinute}-${now.getSecond}.txt"
+    def getFilePath = exchangeFolder + fileNameFromDate(LocalDateTime.now)
+
+    def fileNameFromDate(d:LocalDateTime) = 
+        s"${d.getYear}-${intToStr(d.getMonthValue, 2)}-${intToStr(d.getDayOfMonth, 2)}" +
+        s"_${intToStr(d.getHour, 2)}-${intToStr(d.getMinute, 2)}-${intToStr(d.getSecond, 2)}.txt"
+
+    def intToStr(n:Int, lengt:Int) = {
+        val str = n.toString
+        str.length match {
+            case less if less < lengt => "0" * (lengt - less) + str
+            case _ => str
+        }
     }
 }
